464773b48f8d12da3fd3d0855df48b4a19e5c53d
Thieslei Oliveira <thieslei@gmail.com>
2021-09-27 12:28:58 -0300

    change script calculo de notas

--- calculo_nota_aprovado_ou_nao.py
+++ calculo_nota_aprovado_ou_nao.py
@@ -1,3 +1,16 @@
+### Este script tem como objetivo:
+# - Ler 2 notas
+# - Calcular a média
+# - E mostrar o resultado na tela em relação a aprovação ou não do aluno.
+# - Se nota menor que 5 = REPROVADO
+# - Se nota maior que 5 e menor que 6.9 = RECUPERAÇÃO 
+# - Se nota maior que 7 = APROVADO
+#
+#
+#
+#
+### Autor thieslei@gmail.com
+
 nota1 = float(input('Digite a sua primeira nota: '))
 nota2 = float(input('Digite a sua segunda nota: '))
 media = (nota1 + nota2) / 2
